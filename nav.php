bfed691bbc93cf83fec29c7e47e13c348f3c44ad
Juan <jereyess98@gmail.com>
2020-08-07 19:16:05 -0500

    modificador

---
+++ nav.php
@@ -0,0 +1,9 @@
+<nav>
+	<ol>
+		<li><a href="#">Hola</a></li>
+		<li><a href="#">que pasa</a></li>
+		<li><a href="#">quien son</a></li>
+		<li><a href="#">donde estan</a></li>
+		<li><a href="#">donde</a></li>
+	</ol>
+</nav>
