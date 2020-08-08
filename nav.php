70d4af8f46b1e3faed8158dae82795fe9f18b31a
saraitry <69371649+saraitry@users.noreply.github.com>
2020-08-07 19:09:38 -0500

    Seguimos donde mismo

--- nav.php
+++ nav.php
@@ -1,1 +1,16 @@
-# segundo-repo
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+  <h1>hola que pasa</h1>
+  <ul>
+	<li>inicio</li>
+	<li>contacto</li>
+	<li>registro</li>
+	<li>salir</li>
+</ul>
+</body>
+</html>
